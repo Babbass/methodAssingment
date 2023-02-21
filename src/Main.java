918383cb58e9fbe46649d5b2a0282122d2413767
Babbass <Jaycee4005@gmail.com>
2023-02-21 11:09:18 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -17,13 +17,14 @@ class CalculateG {
                 + finalPosition + " m.");
 // Add output line for velocity (similar to position)
         System.out.println("The object's velocity after" + fallingTime + "second is" + finalVelocity + "m.");
-
+//calling the methods in the main class
 Multiplication();
 Square();
 Summation();
     }
 
     public static void Multiplication() {
+        //multiplication variables
         int Num1;
         int Num2;
         int Multiplication;
@@ -35,22 +36,26 @@ Summation();
         Num1 = Multiplicationinput.nextInt();
         System.out.println("Enter the Second number:");
         Num2 = Multiplicationinput.nextInt();
+        //multiply Num1 by Num2
         Multiplication = Num1 * Num2;
         System.out.println("Multiplication answer is:"+Multiplication);
 
     }
 
      public static void Square() {
+        //square variables
          int Num1;
          int Num2;
          int square;
          Scanner squareinput = new Scanner(System.in);
          System.out.println("Enter the first number:");
          Num1 = squareinput.nextInt();
+         //multilpy Num1 variable by itself to get the square
          square = Num1 * Num1;
          System.out.println("The square answer is:"+square);
      }
      public  static void Summation() {
+        //sum variables
         int Num1;
         int Num2;
         int Sum;
@@ -59,6 +64,7 @@ Summation();
          Num1=Suminput.nextInt();
          System.out.println("Enter the Second number:");
          Num2=Suminput.nextInt();
+         //adding the two variables
          Sum=Num1+Num2;
          System.out.println("The summation answer is:"+Sum);
     }
